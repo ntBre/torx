e20ed7084598f8b2a257fe90e28e8c9f6a5268ec
Brent Westbrook <brentrwestbrook@gmail.com>
2023-09-28 14:32:07 -0400

    initial commit, steal graph impl from msm crate

---
+++ src/lib.rs
@@ -0,0 +1,65 @@
+use std::collections::{BTreeMap, BTreeSet, HashSet};
+
+#[derive(Debug)]
+pub struct Graph {
+    /// adjacency list?
+    adj: BTreeMap<usize, BTreeSet<usize>>,
+    nodes: BTreeSet<usize>,
+}
+
+impl Graph {
+    pub fn new() -> Self {
+        Self {
+            adj: BTreeMap::new(),
+            nodes: BTreeSet::new(),
+        }
+    }
+
+    pub fn add_node(&mut self, node: usize) {
+        self.adj.entry(node).or_insert(BTreeSet::new());
+        self.nodes.insert(node);
+    }
+
+    pub fn add_edge(&mut self, i: usize, j: usize) {
+        // for some unholy reason they don't just call add_node here
+        self.add_node(i);
+        self.add_node(j);
+        // this is pretty strictly unnecessary, but I guess it's okay. I
+        // should be able to get_mut instead since I know the or_insert
+        // already happened in add_node
+        self.adj
+            .entry(i)
+            .and_modify(|h| {
+                h.insert(j);
+            })
+            .or_insert(BTreeSet::new());
+        self.adj
+            .entry(j)
+            .and_modify(|h| {
+                h.insert(i);
+            })
+            .or_insert(BTreeSet::new());
+    }
+
+    pub fn edges(&self) -> Vec<(usize, usize)> {
+        let mut seen = HashSet::new();
+        let mut ret = Vec::new();
+        for (n, nbrs) in &self.adj {
+            for nbr in nbrs {
+                if !seen.contains(&nbr) {
+                    ret.push((*n, *nbr));
+                }
+            }
+            seen.insert(n);
+        }
+        ret
+    }
+
+    pub fn nodes(&self) -> impl Iterator<Item = &usize> {
+        self.nodes.iter()
+    }
+
+    pub fn neighbors(&self, node: &usize) -> impl Iterator<Item = &usize> {
+        self.adj[node].iter()
+    }
+}
